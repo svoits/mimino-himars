7ea0bd2ed6943e045add82b5094e0dfad52c4ade
Mikaeri <klaus.com82@gmail.com>
2023-01-29 14:06:16 +0200

    up swiper

--- src/index.js
+++
@@ -1,1 +0,0 @@
-import './js/dh-swiper'
--- src/js/dh-swiper.js
+++ src/js/dh-swiper.js
@@ -1,12 +1,9 @@
 var swiper = new Swiper(".dh-swiper", {//в лапках пишемо назву вашого контейнеру
-      slidesPerView: 1.5, //кількість об'єктів що відображаються
+      slidesPerView: 1.25, //кількість об'єктів що відображаються
       cssMode: true,
       navigation: {
         nextEl: ".dh-slide-next",// в лапках ставимо клас об'єкта при на тисненні на який переключається слайд
         prevEl: ".dh-slide-prev",
-      },
-      pagination: {
-        el: ".dh-swiper-pagination", //пагінація якщо є
       },
       mousewheel: true,//прокрутка мишкою
       keyboard: true,//прокрутка клавою
