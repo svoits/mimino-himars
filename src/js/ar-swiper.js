7e7ca9095a94bb1b814ce70a992ab9755b56ce44
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-01-29 17:15:22 +0100

    add swiper css

--- src/js/ar-swiper.js
+++ src/js/ar-swiper.js
@@ -1,12 +1,38 @@
 var swiper = new Swiper('.ar-swiper', {
   slidesPerView: 1.5,
-  cssMode: true,
+  spaceBetween: 25,
+  breakpoints: {
+    414: {
+      slidesPerView: 1.5,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1280: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
   loop: true,
-  // autoplay: true,
+  loopFillGroupWithBlank: true,
+
+  cssMode: true,
   navigation: {
     nextEl: '.ar-swiper-right',
     prevEl: '.ar-swiper-left',
   },
-  mousewheel: true,
-  keyboard: true,
 });
+// var swiper = new Swiper('.ar-swiper', {
+//   //в лапках пишемо назву вашого контейнеру
+//   slidesPerView: 1.5, //кількість об'єктів що відображаються
+//   cssMode: true,
+//   navigation: {
+//     nextEl: '.ar-swiper-right', // в лапках ставимо клас об'єкта при на тисненні на який переключається слайд
+//     prevEl: '.ar-swiper-left',
+//   },
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   mousewheel: true, //прокрутка мишкою
+//   keyboard: true, //прокрутка клавою
+// });
