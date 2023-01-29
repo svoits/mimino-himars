aa4a857072f7599c3e472bfc3b726ac3e8adda5a
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-01-29 17:49:06 +0100

    fix slider

--- src/js/ar-swiper.js
+++ src/js/ar-swiper.js
@@ -1,26 +1,25 @@
 var swiper = new Swiper('.ar-swiper', {
   slidesPerView: 1.5,
   spaceBetween: 25,
+
+  loop: true,
+  loopFillGroupWithBlank: true,
+
+  // cssMode: true,
+  navigation: {
+    nextEl: '.ar-swiper-right',
+    prevEl: '.ar-swiper-left',
+  },
+
   breakpoints: {
     414: {
-      slidesPerView: 1.5,
+      slidesPerView: 1.25,
+      spaceBetween: 25,
     },
     1024: {
       slidesPerView: 2,
       spaceBetween: 40,
     },
-    1280: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-  },
-  loop: true,
-  loopFillGroupWithBlank: true,
-
-  cssMode: true,
-  navigation: {
-    nextEl: '.ar-swiper-right',
-    prevEl: '.ar-swiper-left',
   },
 });
 // var swiper = new Swiper('.ar-swiper', {
