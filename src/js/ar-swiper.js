ddc1ac45335603c50a19ac05eec6723ab1517f4f
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-01-30 21:31:57 +0100

    fixed swiper

--- src/js/ar-swiper.js
+++ src/js/ar-swiper.js
@@ -3,8 +3,8 @@ var swiper = new Swiper('.ar-swiper', {
   spaceBetween: 25,
 
   loop: true,
-  loopedSlides: 4,
-  loopFillGroupWithBlank: true,
+  // loopedSlides: 4,
+  // loopFillGroupWithBlank: true,
 
   navigation: {
     nextEl: '.ar-swiper-prew',
@@ -12,15 +12,7 @@ var swiper = new Swiper('.ar-swiper', {
   },
 
   breakpoints: {
-    414: {
-      slidesPerView: 1.25,
-      spaceBetween: 25,
-    },
     1024: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-    1280: {
       slidesPerView: 2,
       spaceBetween: 40,
     },
