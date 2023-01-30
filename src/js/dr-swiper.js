9c307da6c2b4326bb3ce189acef47a22a2d343db
Youneri <118117711+Youneri@users.noreply.github.com>
2023-01-30 21:00:03 +0200

    fixed swiper

--- src/js/dr-swiper.js
+++ src/js/dr-swiper.js
@@ -1,26 +1,25 @@
-var swiper = new Swiper(".details-restaurant-swiper-container", {
-      slidesPerView: 1.5,
-      spaceBetween: 20,
-      
+var swiper = new Swiper('.details-restaurant__swiper-container', {
+  slidesPerView: 1.5,
+  spaceBetween: 20,
+
   breakpoints: {
-    // when window width is >= 320px
     414: {
       slidesPerView: 1.5,
-      spaceBetween: 20
+      spaceBetween: 20,
     },
     1024: {
-      slidesPerView: 2.2,
-      spaceBetween: 34
+      slidesPerView: 3,
+      spaceBetween: 34,
     },
     1280: {
-      slidesPerView: 2.5,
-      spaceBetween: 33
-     }
+      slidesPerView: 3,
+      spaceBetween: 33,
     },
-      loop: true,
-      
-      navigation: {
-        nextEl: ".details-restaurant-next",
-        prevEl: ".details-restaurant-prev",
-      },
-    });
+  },
+  loop: true,
+
+  navigation: {
+    nextEl: '.details-restaurant__next',
+    prevEl: '.details-restaurant__prev',
+  },
+});
