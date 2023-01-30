9619255fb124097857ba87e5d845fbf3385269c8
talask <natasha169@ukr.net>
2023-01-30 02:22:28 +0200

    fixed styles

--- src/js/swiper-large-suite.js
+++ src/js/swiper-large-suite.js
@@ -1,13 +1,11 @@
 var swiper = new Swiper(".large-suite-gallery", {
-    slidesPerView: 1.5,
-    spaceBetween: 30,
     loop: true,
     loopFillGroupWithBlank: true,
 
     breakpoints: {
       // when window width is >= 414px
       414: {
-        slidesPerView: 1.5,
+        slidesPerView: 1.3,
         spaceBetween: 20
       },
       // when window width is >= 1024px
