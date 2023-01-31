528b680252ce14ffd8096699c3809e88726b6ce6
svoits <82117485+svoits@users.noreply.github.com>
2023-01-31 23:54:13 +0000

    Deploying to gh-pages from @ svoits/mimino-himars@1f3b398f25c9ceb1bedb5b66724ff7b380d14c0a 🚀

---
+++ index.1cdc8cd0.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".dh-swiper",{slidesPerView:-1.25,spaceBetween:20,loop:!0,loopFillGroupWithBlank:!0,variableWidth:!0,autoplay:!0,autoplaySpeed:2e3,navigation:{nextEl:".dh-slide-next",prevEl:".dh-slide-prev"},breakpoints:{1024:{slidesPerView:3,spaceBetween:34},1280:{slidesPerView:2,spaceBetween:86}}});
+//# sourceMappingURL=index.1cdc8cd0.js.map
---
+++ index.7fe76ed1.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".details-restaurant__swiper-container",{slidesPerView:1.25,spaceBetween:20,breakpoints:{414:{slidesPerView:1.25,spaceBetween:20},1024:{slidesPerView:2.25,spaceBetween:34},1280:{slidesPerView:3,spaceBetween:86}},loop:!0,navigation:{nextEl:".details-restaurant__next",prevEl:".details-restaurant__prev"}});
+//# sourceMappingURL=index.7fe76ed1.js.map
