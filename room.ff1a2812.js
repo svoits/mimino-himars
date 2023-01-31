8dddf43daadb0a7366c4c4eb11d49820893ac2dc
svoits <82117485+svoits@users.noreply.github.com>
2023-01-31 18:40:53 +0000

    Deploying to gh-pages from @ svoits/mimino-himars@751cffa3c78da0526c965bd48bf65412b0a95fed 🚀

---
+++ room.ff1a2812.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".hero-slider").slick({infinite:!0,arrows:!0,speed:300,cssEase:"linear",variableWidth:!0,centerMode:!0,autoplay:!0,autoplaySpeed:3e3})}));
+//# sourceMappingURL=room.ff1a2812.js.map
