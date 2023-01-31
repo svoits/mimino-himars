af859f779893f9699343541973536b23e87111a3
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-02-01 00:44:31 +0100

    fix

--- src/js/dh-swiper.js
+++ src/js/dh-swiper.js
@@ -1,26 +1,29 @@
-  var swiper = new Swiper(".dh-swiper", {
-      slidesPerView: -1.25,
-      // cssMode: true,
-      spaceBetween: 20,
-      loop: true,
-      loopFillGroupWithBlank: true, 
-      variableWidth: true,
-      
-      navigation: {
-      nextEl: ".dh-slide-next",
-      prevEl: ".dh-slide-prev",
-    },
-      
-    // Responsive breakpoints
+var swiper = new Swiper('.dh-swiper', {
+  slidesPerView: -1.25,
+  // cssMode: true,
+  spaceBetween: 20,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  variableWidth: true,
+
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+  navigation: {
+    nextEl: '.dh-slide-next',
+    prevEl: '.dh-slide-prev',
+  },
+
+  // Responsive breakpoints
   breakpoints: {
     // when window width is >= 1024px
     1024: {
       slidesPerView: 3,
-      spaceBetween: 34
+      spaceBetween: 34,
     },
     1280: {
       slidesPerView: 2,
-      spaceBetween: 86
+      spaceBetween: 86,
     },
-  }
-    });
+  },
+});
