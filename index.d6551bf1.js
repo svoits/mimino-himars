18b0d3a903829fb62ef773d6a151076cd84dd5da
svoits <82117485+svoits@users.noreply.github.com>
2023-01-31 23:45:10 +0000

    Deploying to gh-pages from @ svoits/mimino-himars@af859f779893f9699343541973536b23e87111a3 🚀

--- index.d6551bf1.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".dh-swiper",{slidesPerView:-1.25,spaceBetween:20,loop:!0,loopFillGroupWithBlank:!0,variableWidth:!0,navigation:{nextEl:".dh-slide-next",prevEl:".dh-slide-prev"},breakpoints:{1024:{slidesPerView:3,spaceBetween:34},1280:{slidesPerView:2,spaceBetween:86}}});
-//# sourceMappingURL=index.d6551bf1.js.map
