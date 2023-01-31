7f4919030cc4c6025c708604723b2449577f2a5d
Mikaeri <klaus.com82@gmail.com>
2023-01-31 02:07:44 +0200

    Finished styles and html
    Finished styles and html, but the swiper is not working now in the commit

--- src/js/dh-swiper.js
+++ src/js/dh-swiper.js
@@ -1,44 +1,26 @@
-// var swiper = new Swiper(".dh-swiper", {//в лапках пишемо назву вашого контейнеру
-//       slidesPerView: 1.25, //кількість об'єктів що відображаються
-//       // cssMode: true,
-//       navigation: {
-//         nextEl: ".dh-slide-next",// в лапках ставимо клас об'єкта при на тисненні на який переключається слайд
-//         prevEl: ".dh-slide-prev",
-//       },
-
-//       mousewheel: true,//прокрутка мишкою
-//       keyboard: true,//прокрутка клавою
-// });
-    
-var swiper = new Swiper(".dh-swiper", {
-  slidesPerView: 1.5,
-  // cssMode: true,
-  // swiperProgress: 0.5,
-      calculateHeight:true,
-      spaceBetween: 30,
+  var swiper = new Swiper(".dh-swiper", {
+      slidesPerView: -1.25,
+      // cssMode: true,
+      spaceBetween: 20,
       loop: true,
-  loopFillGroupWithBlank: true,
-      setWrapperSize: true,
-      // reverseDirection: true,
+      loopFillGroupWithBlank: true, 
       variableWidth: true,
+      
       navigation: {
-        nextEl: ".dh-slide-next",
-        prevEl: ".dh-slide-prev",
-  },
-       // Default parameters
-  slidesPerView: 1,
-  spaceBetween: 10,
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 414px
-    414: {
-      slidesPerView: 1.6,
-      spaceBetween: 20
+      nextEl: ".dh-slide-next",
+      prevEl: ".dh-slide-prev",
     },
+      
+    // Responsive breakpoints
+  breakpoints: {
     // when window width is >= 1024px
     1024: {
       slidesPerView: 3,
-      spaceBetween: 88
+      spaceBetween: 34
+    },
+    1280: {
+      slidesPerView: 2,
+      spaceBetween: 86
     },
   }
     });
