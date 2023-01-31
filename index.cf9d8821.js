83fdf3c4eeb0da6e2c985480e68e35226fba3bca
svoits <82117485+svoits@users.noreply.github.com>
2023-01-31 21:00:23 +0000

    Deploying to gh-pages from @ svoits/mimino-himars@7fbebe2f26e36f28fab113261fead5690c04c7b1 🚀

---
+++ index.cf9d8821.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".about-hotel-swiper",{slidesPerView:1.32,spaceBetween:20,loop:!0,loopFillGroupWithBlank:!0,autoplay:!0,autoplaySpeed:2e3,navigation:{nextEl:".ah-swiper-right",prevEl:".ah-swiper-left"},breakpoints:{1024:{slidesPerView:2,spaceBetween:40}}});
+//# sourceMappingURL=index.cf9d8821.js.map
