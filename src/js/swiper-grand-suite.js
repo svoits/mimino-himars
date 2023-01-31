5b74f1fff0035bfea7a9633efba02c19a20abc00
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-01-31 04:00:03 +0100

    some fix

--- src/js/swiper-grand-suite.js
+++ src/js/swiper-grand-suite.js
@@ -1,23 +1,19 @@
-var swiper = new Swiper(".grand-suite-gallery", {
-    loop: true,
-    loopFillGroupWithBlank: true,
+var swiper = new Swiper('.grand-suite-gallery', {
+  loop: true,
+  loopFillGroupWithBlank: true,
+  slidesPerView: 1.53,
+  spaceBetween: 16,
 
-    breakpoints: {
-      // when window width is >= 414px
-      414: {
-        slidesPerView: 1.3,
-        spaceBetween: 20
-      },
-      // when window width is >= 1024px
-      1024: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      }
+  breakpoints: {
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
+  },
 
-    navigation: {
-      nextEl: ".grand-arrow-next",
-      prevEl: ".grand-arrow-prev",
-    },
-
+  navigation: {
+    nextEl: '.grand-arrow-next',
+    prevEl: '.grand-arrow-prev',
+  },
 });
