7e01a84d7537c2367306cb24b434affad1747106
Bandera77 <arnayt.20@gmail.com>
2023-01-31 02:46:46 +0100

    fixed width swiper

--- src/js/ah-swiper.js
+++ src/js/ah-swiper.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper('.about-hotel-swiper', {
-  slidesPerView: 1.35,
-  spaceBetween: 10,
+  slidesPerView: 1.32,
+  spaceBetween: 20,
 
   loop: true,
   loopFillGroupWithBlank: true,
@@ -14,10 +14,6 @@ var swiper = new Swiper('.about-hotel-swiper', {
   },
 
   breakpoints: {
-    360: {
-      slidesPerView: 1.32,
-      spaceBetween: 20,
-    },
     1024: {
       slidesPerView: 2,
       spaceBetween: 40,
