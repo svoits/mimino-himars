d6b7460493d649ddb14d2e8465e2bc54caa2224f
talask <natasha169@ukr.net>
2023-02-01 02:17:19 +0200

    update styles

--- src/js/swiper-classic-suite.js
+++ src/js/swiper-classic-suite.js
@@ -1,13 +1,13 @@
 var swiper = new Swiper(".classic-suite-gallery", {
-    slidesPerView: 1.31,
-    spaceBetween: 16,
+    // slidesPerView: 1.31,
+    // spaceBetween: 16,
     loop: true,
     loopFillGroupWithBlank: true,
   
     breakpoints: {
       // when window width is >= 1024px
       414: {
-        slidesPerView: 1.3,
+        slidesPerView: 1.5,
         spaceBetween: 16
       },
       // when window width is >= 1024px
