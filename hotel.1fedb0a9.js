788cd06a28b34d9ebbbe2d148f37d812e3563d8e
svoits <82117485+svoits@users.noreply.github.com>
2023-02-01 00:16:00 +0000

    Deploying to gh-pages from @ svoits/mimino-himars@3561faab462710f8f25952023c4eb0e6f919827c 🚀

---
+++ hotel.1fedb0a9.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".large-suite-gallery",{slidesPerView:1.25,spaceBetween:16,loop:!0,loopFillGroupWithBlank:!0,breakpoints:{414:{slidesPerView:1.3,spaceBetween:16},1024:{slidesPerView:2,spaceBetween:40}},navigation:{nextEl:".large-arrow-next",prevEl:".large-arrow-prev"}});
+//# sourceMappingURL=hotel.1fedb0a9.js.map
