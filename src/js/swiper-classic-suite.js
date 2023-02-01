b752212b6457cbc5f0abce4e64b80f11258c86ae
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-02-01 02:28:06 +0100

    fix styles

--- src/js/swiper-classic-suite.js
+++ src/js/swiper-classic-suite.js
@@ -1,25 +1,22 @@
-var swiper = new Swiper(".classic-suite-gallery", {
-    // slidesPerView: 1.31,
-    // spaceBetween: 16,
-    loop: true,
-    loopFillGroupWithBlank: true,
-  
-    breakpoints: {
-      // when window width is >= 1024px
-      414: {
-        slidesPerView: 1.5,
-        spaceBetween: 16
-      },
-      // when window width is >= 1024px
-      1024: {
-        slidesPerView: 2,
-        spaceBetween: 40
-      }
+var swiper = new Swiper('.classic-suite-gallery', {
+  slidesPerView: 1.53,
+  spaceBetween: 16,
+
+  loop: true,
+  loopFillGroupWithBlank: true,
+
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+  navigation: {
+    nextEl: '.classic-arrow-next',
+    prevEl: '.classic-arrow-prev',
+  },
+
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
-  
-    navigation: {
-      nextEl: ".classic-arrow-next",
-      prevEl: ".classic-arrow-prev",
-    },
-  
-  });
+  },
+});
