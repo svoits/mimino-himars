f9507878c0a1624bcdebe3fa8b0b488a34dfd8b6
svoits <82117485+svoits@users.noreply.github.com>
2023-02-01 00:42:42 +0000

    Deploying to gh-pages from @ svoits/mimino-himars@71ac821c9f378d24558da4f8350850b817e3d202 🚀

---
+++ restaurant.35329f98.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".ak-swiper",{slidesPerView:1.315,spaceBetween:25,loop:!0,autoplay:!0,autoplaySpeed:2e3,navigation:{nextEl:".ak-swiper-next",prevEl:".ak-swiper-prev"},breakpoints:{1024:{slidesPerView:2,spaceBetween:40}}});
+//# sourceMappingURL=restaurant.35329f98.js.map
