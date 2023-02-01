6c8588345ced55fc1f29a82e7cc538b5ee9fbb01
cutestsun <118314867+cutestsun@users.noreply.github.com>
2023-02-01 01:33:59 +0000

    Deploying to gh-pages from @ svoits/mimino-himars@1deb98f059473b3a460fdb0ce35588b4f20bfb9d 🚀

---
+++ hotel.b1b04a99.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".classic-suite-gallery",{slidesPerView:1.53,spaceBetween:16,loop:!0,loopFillGroupWithBlank:!0,autoplay:!0,autoplaySpeed:2e3,navigation:{nextEl:".classic-arrow-next",prevEl:".classic-arrow-prev"},breakpoints:{1024:{slidesPerView:2,spaceBetween:40}}});
+//# sourceMappingURL=hotel.b1b04a99.js.map
