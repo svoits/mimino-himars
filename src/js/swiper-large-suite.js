95cd1d5890f97e2d0fb12f2f9d8f41757e6f7bff
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-02-02 10:17:58 +0100

    hotel page swiper fixes

--- src/js/swiper-large-suite.js
+++ src/js/swiper-large-suite.js
@@ -1,25 +1,22 @@
-var swiper = new Swiper(".large-suite-gallery", {
-  slidesPerView: 1.25,
+var swiper = new Swiper('.large-suite-gallery', {
+  slidesPerView: 1.42,
   spaceBetween: 16,
+
   loop: true,
   loopFillGroupWithBlank: true,
 
-  breakpoints: {
-    // when window width is >= 1024px
-    414: {
-      slidesPerView: 1.3,
-      spaceBetween: 16
-    },
-    // when window width is >= 1024px
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 40
-    }
-  },
+  autoplay: true,
+  autoplaySpeed: 2000,
 
   navigation: {
-    nextEl: ".large-arrow-next",
-    prevEl: ".large-arrow-prev",
+    nextEl: '.large-arrow-next',
+    prevEl: '.large-arrow-prev',
   },
 
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
 });
