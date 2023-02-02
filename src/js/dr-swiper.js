79234315e3ceee9ad3ef41f97e9059ae25f371e4
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-02-02 20:35:05 +0100

    fix js

--- src/js/dr-swiper.js
+++ src/js/dr-swiper.js
@@ -1,21 +1,21 @@
-var swiper = new Swiper(".details-restaurant__swiper-container", {
-      slidesPerView: "auto",
-      loop: true,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      loopFillGroupWithBlank: true,
-      spaceBetween: 20,
-            navigation: {
-      nextEl: '.details-restaurant__next',
-      prevEl: '.details-restaurant__prev',
-      },
-     breakpoints: {
-//     // when window width is >= 1024px
+var swiper = new Swiper('.details-restaurant__swiper-container', {
+  slidesPerView: 'auto',
+  loop: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  // loopFillGroupWithBlank: true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.details-restaurant__next',
+    prevEl: '.details-restaurant__prev',
+  },
+  breakpoints: {
+    //     // when window width is >= 1024px
     1024: {
       spaceBetween: 34,
     },
     1280: {
-      spaceBetween: 86,
+      spaceBetween: 70,
     },
   },
-    });
+});
